090fcb001d667c6e41466aff311ef6b7f42d697d
marcos mayorga <other.arkitech@protonmail.com>
2018-06-15 10:46:15 +0100

    moved impl to cpp file

--- gov/cfg.h
+++ gov/cfg.h
@@ -1,55 +1,24 @@
 #ifndef USGOV_cecc5cb611fac4022cc60e3a9caee608906e8b3256ea18a917c439b57efff781
 #define USGOV_cecc5cb611fac4022cc60e3a9caee608906e8b3256ea18a917c439b57efff781
 
-#include "crypto.h"
-#include <fstream>
-#include <sys/stat.h>
-#include <cstdlib>
-
 namespace us { namespace gov {
 namespace filesystem {
 using namespace std;
 
 struct cfg {
-	cfg() {
-	}
-	virtual ~cfg() {
-	}
+	cfg();
+	virtual ~cfg();
 
-	static bool mkdir(const string& d) {
-		int e = ::mkdir(d.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-		if (e==-1) cerr << errno << " " << strerror(errno) << endl;
-		return e!=-1;
-	}
-	static bool file_exists(const string& f) {
-	    struct stat s;
-	    stat(f.c_str(), &s);
-	    return S_ISREG(s.st_mode);
-	}
-	static bool ensure_dir(const string& d) {
-		if (!exists_dir(d)) {
-            const int dir_err = system(string("mkdir -p ")+d);
-            if (dir_err == -1) {
-                cerr << "Error creating directory " << d << endl;
-                return false;
-            }
-			return true;
-		}
-	    return true;
-	}
-	static bool exists_dir(const string& d) {
-	    struct stat s;
-	    stat(d.c_str(), &s);
-	    return S_ISDIR(s.st_mode);
-	}
-
-	static string abs_file(const string& home, const string& fn) { return home+"/"+fn; }
+	static bool mkdir(const string& d);
+	static bool file_exists(const string& f);
+	static bool ensure_dir(const string& d);
+	static bool exists_dir(const string& d);
+	static string abs_file(const string& home, const string& fn);
 
 };
 
 }
-}
-}
+}}
 
 #endif
 
