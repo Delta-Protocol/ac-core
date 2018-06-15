ae0ca1f3b99a597ac6dc96777044585dfb979129
marcos mayorga <other.arkitech@protonmail.com>
2018-06-15 10:48:31 +0100

    moved impl to cpp file

---
+++ gov/cfg.cpp
@@ -0,0 +1,50 @@
+#include "cfg.h"
+//#include "crypto.h"
+#include <fstream>
+#include <sys/stat.h>
+#include <cstdlib>
+
+using namespace std;
+using namespace us::gov;
+typedef us::gov::cfg c;
+
+c::cfg() {
+}
+
+virtualc:: ~cfg() {
+}
+
+bool mkdir(const string& d) {
+	int e = ::mkdir(d.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+	if (e==-1) cerr << errno << " " << strerror(errno) << endl;
+	return e!=-1;
+}
+
+ bool c::file_exists(const string& f) {
+    struct stat s;
+    stat(f.c_str(), &s);
+    return S_ISREG(s.st_mode);
+}
+
+bool c::ensure_dir(const string& d) {
+	if (!exists_dir(d)) {
+	        const int dir_err = system(string("mkdir -p ")+d);
+        	if (dir_err == -1) {
+	           cerr << "Error creating directory " << d << endl;
+        	   return false;
+	        }
+		return true;
+	}
+	return true;
+}
+
+bool c::exists_dir(const string& d) {
+    struct stat s;
+    stat(d.c_str(), &s);
+    return S_ISDIR(s.st_mode);
+}
+
+string c::abs_file(const string& home, const string& fn) { 
+	return home+"/"+fn;
+}
+
