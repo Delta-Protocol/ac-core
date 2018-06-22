1b686b002145d33f85c1ba81b3c6e4225e1f92dc
James Kirby <nshcore@protonmail.com>
2018-06-22 16:26:47 +0100

    -

---
+++ test/wallet/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "api.h"
+
+
+using namespace std;
+
+
+int main() {
+
+	//cout << "-----wallet--Main----" << endl;
+ 
+	testing_wallet_api(); //("127.0.0.1" , 46001);
+
+
+
+exit(0);
+}
+
+
