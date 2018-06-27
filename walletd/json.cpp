d6fe1d25d22cc57cd91875f5bb5ee3c75804f192
marcos mayorga <other.arkitech@protonmail.com>
2018-06-27 08:46:22 +0100

    -

--- walletd/json.cpp
+++ walletd/json.cpp
@@ -5,8 +5,11 @@ using namespace us::wallet::w3api;
 typedef us::wallet::w3api::json c;
  
 Json::Value c::convert_response_new_compartiment(const string& s) {
+    istringstream is(s);
+    string p;
+    is >> p;
     Json::Value val;
-    val["compartiment"]=s;
+    val["compartiment"]=p;
     return val;
 }
 
