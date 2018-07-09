1d7d3df6fe031290f34304a1d7117e99fc4f62a7
marcos mayorga <other.arkitech@protonmail.com>
2018-07-09 13:16:04 +0100

    test symmetric_encryption mockup

---
+++ test/symmetric_encryption/main.cpp
@@ -0,0 +1,34 @@
+#include <us/gov/crypto/symmetric_encryption.h>
+
+using namespace std;
+using namespace us::gov;
+
+
+int main() {
+
+
+
+
+//ec::keys k1=ec::keys::generate();
+
+
+keys::priv_t <== taken from cmdline args
+keys::pub_t  <== taken from cmdline args
+command
+message  <== taken from cmdline args
+
+
+if comand==send {
+   //cypher message
+   //symmetric_encryption o(const keys::priv_t&, const keys::pub_t&);
+
+   prints encrypted payload to console
+}
+if (cmd==recv) {
+   prints decoded to stdout
+}
+
+
+
+
+}
