81982446c12706dbcdb7128d852929fc5a61d17b
marcos mayorga <other.arkitech@protonmail.com>
2018-07-09 07:00:43 +0100

    encryption review

--- gov/crypto/symmetric_encryption.h
+++ gov/crypto/symmetric_encryption.h
@@ -18,24 +18,21 @@ using namespace std;
 
 typedef crypto::ec::keys keys;
 
-    class symmetric_encryption{
-
+    class symmetric_encryption {
         AutoSeededRandomPool prng_;
         unsigned char key_[AES::DEFAULT_KEYLENGTH];
         unsigned char iv_[ AES::BLOCKSIZE ];
         const size_t size_iv = sizeof(iv_);
         const int tag_size_ = 16;
-        void set_agreed_key_value(const keys::priv_t&, const keys::pub_t&);
+        bool set_agreed_key_value(const keys::priv_t&, const keys::pub_t&);
         void set_iv_from_ciphertext(const vector<unsigned char>&);
-        
+
         public:
             symmetric_encryption(const keys::priv_t&, const keys::pub_t&);
             const vector<unsigned char> encrypt(const vector<unsigned char>&);
             const vector<unsigned char> decrypt(const vector<unsigned char>&);     //returns an empty vector<unsigned char> if decrypt is unsuccessful.
-
         };
 
-    
 }
 }}
 #endif
