75fa913a681e69aa18d482107a6246fa11fb90c7
marcos mayorga <other.arkitech@protonmail.com>
2018-07-31 22:36:22 +0100

    -

--- sdk/java/src/gov/socket/client.java
+++ sdk/java/src/gov/socket/client.java
@@ -1,24 +1,29 @@
 package us.gov.socket;
 
 import java.net.Socket;
+import java.io.IOException;
 
 	public class client {
-        public client() {
-//            sock=0;
+
+
+		public client() {
+            sock=null;
         }
 
-		client();
-		client(Socket sock);
+		public client(Socket s) {
+            sock=s;
+            if (connected()) addr=address();
+        }
 /*
 		virtual ~client() {
 			disconnect();
 			  //you must override destructor call the virtual function disconnect on the most specialized class
 		}
 */
-		virtual String connect(String host, int port, boolean block=false) {
+		String connect(String host, int port) {
         	//lock_guard<mutex> lock(mx);
             try {
-                Socket s = new Socket(walletd_host(), walletd_port());
+                sock = new Socket(host, port);
     /*
             	String r=init_sock(host, port, block);
             	if (r!=null) {
@@ -27,23 +32,24 @@ import java.net.Socket;
 */
             }
             catch (IOException e) {
-                return "
+                return new String("Error. "+e.getMessage());
             }
         	addr=host;
             on_connect();
         	return null;
         }
 
-		virtual void disconnect() {
+		void disconnect() {
         //	lock_guard<mutex> lock(mx);
         	if (!connected()) return;
         	sock.close();
         }
 
-        inline boolean connected() const { return sock.isConnected(); }
+        boolean connected() { return sock.isConnected(); }
 
 		String address() {
-            return sock.getHostAddress().getHostAddress();
+//            return sock.getHostAddress().getHostAddress();
+            return new String("");
         }
 
 		void ready() {}
@@ -66,20 +72,17 @@ pair<String,datagram> recv(short expected_service) {
     }
     datagram d=r.second;
     while(true) { ///delete garbage injected by hackers, discovered when gov trolled wallet with vote mesages
-        if (d.service==protocol_gov_socket_error) {
+        if (d.service==protocol.protocol_gov_socket_error) {
             return r;
         }
         if (d.service==expected_service) {
             break;
         }
-        else {
-            delete d;
+        pair<String,datagram> r2=recv();
+        if (r2.first!=null) {
+            return r2;
         }
-        pair<String,datagram> r=recv();
-        if (r.first!=null) {
-            return move(r);
-        }
-        d=r.second;
+        d=r2.second;
     }
     return new pair<String,datagram>(null,d);
 }
@@ -110,8 +113,8 @@ String send(datagram d) {
 	if (!connected()) {
 		return new String("Error. Sending datagram before connecting.");
 	}
-	auto r=d.sendto(sock);
-    if (r!=null)) {
+	pair<String,datagram> r=d.sendto(sock);
+    if (r!=null) {
         disconnect();
     }
 	return r;
@@ -151,95 +154,6 @@ pair<String,datagram> recv() { //caller owns the returning object
 		//mutable mutex mx;
 	}
 
-
-typedef us::gov::socket::client c;
-using namespace std;
-using namespace us::gov::socket;
-
-
-c::client(int sock):sock(sock) {
-   if (sock!=0) addr=address();
-}
-
-string c::address() const {
-}
-
-string c::connect(const string& host, uint16_t port, bool block) {
-}
-#include <us/gov/stacktrace.h>
-
-void c::init_sockaddr (struct sockaddr_in *name, const char *hostname,	uint16_t port) {
-	struct hostent *hostinfo;
-	name->sin_family = AF_INET;
-	name->sin_port = htons (port);
-	hostinfo=::gethostbyname(hostname);
-	if (hostinfo == 0) {
-		cerr << "Unknown host " << hostname << endl;
-		exit(EXIT_FAILURE);
-	}
-	name->sin_addr = *(struct in_addr *) hostinfo->h_addr;
-}
-
-#include <fcntl.h>
-#include <chrono>
-#include <thread>
-#include <sstream>
-
-#ifdef SIM
-#else
-string c::init_sock(const string& host, uint16_t port, bool block) {
-	sockaddr_in servername;
-	if (unlikely(sock!=0)) {
-	  sock=0;
-      return "socket is non zero.";
-	}
-	sock=::socket(PF_INET, SOCK_STREAM, 0);// Create the socket.
-	{
-	auto flags = fcntl(sock, F_GETFL, 0);
-	if (!block) flags|=O_NONBLOCK;
-	fcntl(sock,F_SETFL,flags);
-	}
-
-	init_sockaddr (&servername, host.c_str(), port);//Connect to the server.
-	int r=::connect(sock, (struct sockaddr *) &servername, sizeof (servername));
-	if (unlikely(r<0)) {
-		if (errno==EINPROGRESS) {
-            typedef chrono::high_resolution_clock clock;
-            using namespace chrono_literals;
-
-            int error = 0;
-            socklen_t len = sizeof (error);
-            auto t1 = clock::now();
-            struct sockaddr_storage addr;
-            socklen_t lena=sizeof addr;
-            while(true) {
-	            int a=getpeername(sock, (struct sockaddr*)&addr, &lena);
-	            if (a==0) break;
-	            if (chrono::duration_cast<std::chrono::milliseconds>(clock::now() - t1).count()>500) {
-                    return "Error. Timeout obtaining peername";
-                }
-                this_thread::sleep_for(10ms);
-            }
-	     return "";
-		}
-		::close(sock);
-		sock=0;
-        ostringstream os;
-        os << "Connection refused. errno=" << errno;
-        return os.str();
-	}
-    struct timeval tv;
-    tv.tv_sec = 3; //timeout_seconds;
-    tv.tv_usec = 0;
-    setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof tv);
-	return "";
-}
-#endif
-
-
-
-
-}
 
 
 
