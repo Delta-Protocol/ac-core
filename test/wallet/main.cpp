e537fc4ba6e1a0146a4ddd68369db97c8602f5a0
James Kirby <nshcore@protonmail.com>
2018-08-07 12:32:57 +0100

    -

--- test/wallet/main.cpp
+++ test/wallet/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "api.h"
-#include "daemon_api.h"
+//#include "daemon_api.h"
 
 using namespace std;
 
@@ -10,7 +10,7 @@ int main() {
  
 	testing_wallet_api();
 
-	testing_json_daemon_api();
+	//testing_json_daemon_api();
 
 
 	
