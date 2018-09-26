b95b341a7442faed504d16175b3d88e436718329
James Kirby <nshcore@protonmail.com>
2018-09-26 15:26:54 +0100

    add some examples of sdk api

--- sdk/java/test/main.java
+++ sdk/java/test/main.java
@@ -3,6 +3,8 @@ import us.gov.crypto.test.ec;
 import us.gov.crypto.base58;
 import java.security.KeyPair;
 import java.security.*;
+import us.gov.cash.*;
+import us.wallet.*;
 
 public class main {
 
@@ -24,7 +26,7 @@ public class main {
         try {
 
         //KeyPair k=us.gov.crypto.ec.instance.generateKeyPair();
-        PrivateKey privateKey = us.gov.crypto.ec.instance.getPrivateKey(base58.decode("5vvscYyiYzSMxEdPVqsPstB9wugHuanp6ts1FSWAsvFk"));
+        PrivateKey privateKey = us.gov.crypto.ec.instance.getPrivateKey(base58.decode("Gxp2aWgFB6SoyU79LhBfe71EE4yGVRe84tabJsSa7zRF"));
         KeyPair k = us.gov.crypto.ec.instance.generateKeyPair(privateKey);
 
         System.out.println("Private key: " +us.gov.crypto.ec.instance.to_b58(k.getPrivate()));
@@ -32,7 +34,26 @@ public class main {
 
         short port=16673;
         us.wallet.daemon_rpc_api api=new us.wallet.daemon_rpc_api(k,"127.0.0.1",port);
-        api.balance(false,System.out);
+        //api.balance(false,System.out);
+        //api.list(false,System.out);
+        //api.new_address(System.out);
+        //api.add_address("5vvscYyiYzSMxEdPVqsPstB9wugHuanp6ts1FSWAsvFk", System.out);
+        //api.transfer("gU5xx5Cvbm7NpPKz7TvdvBQmzXa",1,System.out);
+        
+        //tx_make_p2pkh_input i = new tx_make_p2pkh_input("gU5xx5Cvbm7NpPKz7TvdvBQmzXa",10,1,tx.sigcode_t.sigcode_all,tx.sigcode_t.sigcode_all,false);
+        //api.tx_make_p2pkh(i, System.out);
+         
+        //api.tx_sign("gU5xx5Cvbm7NpPKz7TvdvBQmzXa", tx.sigcode_t.sigcode_all, tx.sigcode_t.sigcode_all, System.out);
+        //api.tx_send("gU5xx5Cvbm7NpPKz7TvdvBQmzXa", System.out);
+        //api.tx_decode("gU5xx5Cvbm7NpPKz7TvdvBQmzXa", System.out);
+        //api.tx_check("gU5xx5Cvbm7NpPKz7TvdvBQmzXa", System.out);
+        
+        //api.ping(System.out);
+        
+        //api.list_devices(System.out);
+        //api.pair("26smcjHgGsGgvUEUpoCKpUnwHhL5W9y48Wtz7tryn5Yab", "test", System.out);
+        api.unpair("26smcjHgGsGgvUEUpoCKpUnwHhL5W9y48Wtz7tryn5Yab", System.out);
+        System.out.println();
 
         }
         catch(Exception e) {
